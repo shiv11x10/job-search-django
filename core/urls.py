bd1bb3dff52d37220a85e5fa05b16d25721fb6b1
Shivam Agrawal <shivam96.sa@gmail.com>
2020-08-31 18:00:37 +0530

    full working prototype

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import load, HomeView
+from .views import load, home
 
 app_name = 'core'
 urlpatterns = [
     path('load/', load, name='load'),
-    path('', HomeView.as_view(), name='base')
+    path('', home, name='home')
 ]
--- core/views.py
+++ core/views.py
@@ -3,16 +3,50 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
+
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views.generic import ListView
 
 from .models import Job
 
 
-class HomeView(ListView):
-    model = Job
-    paginate_by = 5
-    template_name = 'job_list.html'
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+# class HomeView(ListView):
+#     model = Job
+#     paginate_by = 5
+#     template_name = 'job_list.html'
+
+
+def home(request):
+    job = Job.objects.all()
+
+    types = []
+    for job_item in job:
+        if job_item.type not in types:
+            types.append(job_item.type)
+
+    location = request.GET.get('location')
+    title = request.GET.get('title')
+    job_type = request.GET.get('job_type')
+
+    if is_valid_queryparam(location):
+        job = job.filter(location__icontains=location)
+
+    if is_valid_queryparam(title):
+        job = job.filter(title__icontains=title)
+
+    if is_valid_queryparam(job_type):
+        job = job.filter(type=job_type)
+
+    # paginator = Paginator(job, 5)  # Show 5 contacts per page.
+    #
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+    return render(request, 'job_list.html', {'page_obj': job, 'types': types})
 
 
 def load(request):
